1c669642fb7116b5a825c051a4ad5493949ed6bf
Paulo <paulomsmsm@gmail.com>
2025-03-14 14:57:28 -0300

    aula4

--- my-first-project/src/app/clients/components/client-form/client-form.component.ts
+++ my-first-project/src/app/clients/components/client-form/client-form.component.ts
@@ -24,7 +24,8 @@ export class ClientFormComponent {
 
   @Output() clientSubmited = new EventEmitter<ClientModelForm>();
 
-  obSubmit(_: NgForm) {
+  onSubmit(_: NgForm) {
+    console.log(this.client)
     this.clientSubmited.emit(this.client)
   }
 }
--- my-first-project/src/app/clients/new-client/new-client.component.ts
+++ my-first-project/src/app/clients/new-client/new-client.component.ts
@@ -5,14 +5,18 @@ import { ClientsService } from '../../services/api-client/clients/clients.servic
 import { ClientModelForm } from '../client.models';
 import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
+import { ISnackbarManagerService } from '../../services/isnackbar-manager.service';
+import { SnackbarManagerService } from '../../services/snackbar-manager.service';
+import { ClientFormComponent } from '../components/client-form/client-form.component';
 
 @Component({
   selector: 'app-new-client',
-  imports: [],
+  imports: [ClientFormComponent],
   templateUrl: './new-client.component.html',
-  styleUrl: './new-client.component.css',
+  styleUrls: ['./new-client.component.css'],
   providers: [
-    { provide: SERVICES_TOKEN.HTTP.CLIENT, useClass: ClientsService }
+    { provide: SERVICES_TOKEN.HTTP.CLIENT, useClass: ClientsService },
+    { provide: SERVICES_TOKEN.SNACKBAR, useClass: SnackbarManagerService}
   ]
 })
 export class NewClientComponent implements OnDestroy {
@@ -21,6 +25,7 @@ export class NewClientComponent implements OnDestroy {
 
   constructor(
     @Inject(SERVICES_TOKEN.HTTP.CLIENT)private readonly httpService: IClientService,
+    @Inject(SERVICES_TOKEN.SNACKBAR) private readonly snackBarManager: ISnackbarManagerService,
     private readonly router: Router
   ) {
   }
@@ -32,9 +37,11 @@ export class NewClientComponent implements OnDestroy {
   }
 
   onSubmitClient(value: ClientModelForm) {
+    console.log(value)
     const {id, ...request} = value
     this.httpSubscription = this.httpService.save(request).subscribe(_ => {
-    this.router.navigate(['clients/list'])
+      this.snackBarManager.show('Usuário cadastrado com sucesso')
+      this.router.navigate(['clients/list'])
     })
   }
 
